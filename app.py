2193ba0b8025ae27374f34ffcd84916c138bb338
James Hudibyo <hudibyojames@gmail.com>
2024-01-30 15:42:05 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>Page about {username}</h1>'
+
+#Dynamic routes
+
+
+
