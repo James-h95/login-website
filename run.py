2095d0a6760082b5c1d46e7c011044c4872c880a
James Hudibyo <hudibyojames@gmail.com>
2024-01-31 17:43:16 +0700

    Split app.py file into different python files for each section (routes, models, and a run to start flask app)

---
+++ run.py
@@ -0,0 +1,8 @@
+# Flask APP initialisation. Our Python package
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
