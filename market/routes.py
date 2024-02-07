5ea20065425c54d6e75a66ab3cb779843896716b
James Hudibyo <hudibyojames@gmail.com>
2024-02-07 17:25:54 +0700

    implemented Flash messages feature for all categories in base template. Implemented flash error message upon validation error in register form.

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -20,11 +20,13 @@ def register_page():
     if form.validate_on_submit(): # Verify to execute only after user press submit
         user_to_create = User(username=form.username.data, 
                               email_address=form.email.data, 
-                              password_hash=form.password1.data)  
-        db.session.add(user_to_create)
-        db.session.commit()
+                              password_hash=form.password1.data) 
+
+        with app.app_context:
+            db.session.add(user_to_create)
+            db.session.commit()
         return redirect(url_for('market_page'))
     if form.errors != {}: #if there are errors from validations
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
